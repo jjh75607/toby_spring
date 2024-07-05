d86f5d3be7ca67ef8b36327a810ab1bad2f46593
jjh75607 <dkrak5738@naver.com>
2024-07-06 00:21:04 +0900

    ch01

---
+++ src/main/java/soon/hellotobyspring/ExRateData.java
@@ -0,0 +1,10 @@
+package soon.hellotobyspring;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExRateData(String result, Map<String, BigDecimal> rates) {
+
+}
